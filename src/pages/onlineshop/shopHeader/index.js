6c962706f36eb6d4cfffcc77237f3db7ab2b811e
tiwaripankaj2013 <34410355+tiwaripankaj2013@users.noreply.github.com>
2022-03-16 01:05:19 +0530

    online shop

--- src/pages/onlineshop/shopHeader/index.js
+++ src/pages/onlineshop/shopHeader/index.js
@@ -1,37 +1,24 @@
-import React from 'react';
-
-
- const ShopHeader = () => {
+import React from "react";
+const ShopHeader = () => {
   return (
-     <>
-    <div className="border-t border-b border-gray-400">
-       <select className="max-w-lg px-3 py-2 mx-auto block">
+    <>
+      <div className="border-t border-b border-gray-400">
+        <select className="max-w-lg px-3 py-2 mx-auto block my-3 text-purple-900 outline-none font-medium text-left">
           <option>Supplies by Printer</option>
           <option>Hcl Technology</option>
           <option>Dell Technology</option>
           <option>Hp Technology</option>
-       </select>
-    </div>
-    <div className="py-4 mx-auto text-center mt-6">
-         <h3 className="text-purple-700 text-2xl">Printer, Copier, Fax & Supplies</h3>
-         <p className="text-lg">Apply accoding to the result</p>
-
-         <div className="flex">
-            <div className="w-1/3">
-            <label className="block text-left">Choose By Brand</label>
-            <select className="max-w-lg px-3 py-2 mx-auto block my-1">
-               <option>Supplies by Printer</option>
-               <option>Hcl Technology</option>
-               <option>Dell Technology</option>
-               <option>Hp Technology</option>
-            </select>
-            </div>
-         </div>
-         <br className=" border-b border-gray-500"/>
-    </div>
+        </select>
+      </div>
+      <div className="py-4 mx-auto text-center mt-6">
+        <h3 className="text-purple-700 text-2xl">
+          Printer, Copier, Fax & Supplies
+        </h3>
+        <p className="text-lg">Apply accoding to the result</p>
+      </div>
+      
     </>
-  )
-}
-
+  );
+};
 
 export default ShopHeader;
