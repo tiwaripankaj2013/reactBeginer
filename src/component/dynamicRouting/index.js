5c1d72c7270b1417f6f7ed864e4cc622a0456da0
tiwaripankaj2013 <34410355+tiwaripankaj2013@users.noreply.github.com>
2022-04-30 10:06:56 +0530

    Update index.js

--- src/component/dynamicRouting/index.js
+++ src/component/dynamicRouting/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate,Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const users = [
    {id:"a1", name:'Pankaj',age:24},
    {id:"a2", name:'Sujeet kumar',age:24},
