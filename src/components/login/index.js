f59bc25632f7e577d43ace7038ed146d9aa3c056
tiwaripankaj2013 <34410355+tiwaripankaj2013@users.noreply.github.com>
2022-05-29 23:31:10 +0530

    login form validation on change event

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import { InputWithLabel } from "../ui/input";
 
 const Login = (props) => {
-  const [btnDisable, setBtnEnable] = useState(true);
   const [inputs, setInputs] = useState({
     userName: "",
     userPassword: "",
   });
-  const [err, setErr] = useState(false);
-
+  const [error, setError] = useState(false);
+  const [btnStatus, setBtnStatus] = useState(true);
   const getLoginFormData = (e) => {
+    e.preventDefault();
+    console.log(inputs.userName, inputs.userPassword);
     // console.log(inputs.userName);
     localStorage.setItem(inputs, JSON.stringify(inputs));
   };
@@ -21,10 +22,11 @@ const Login = (props) => {
     });
     // setUName(e.target.value);
     if (inputs.userName.length > 3 && inputs.userPassword.length > 3) {
-      setBtnEnable(false);
-      setErr(false);
+      setError(false);
+      setBtnStatus(false);
     } else {
-      setErr(true);
+      setError(true);
+      setBtnStatus(true);
     }
     // console.log(e.target.value);
   };
@@ -34,40 +36,53 @@ const Login = (props) => {
         <h2 className=" text-blue-900 font-mono text-2xl w-full mb-6">
           {props.formName}
         </h2>
-        <InputWithLabel
-          label="Username"
-          id="username"
-          inputType="text"
-          placeholder="Username"
-          name="userName"
-          value={inputs.userName}
-          onChange={handleChange}
-        />
-        <InputWithLabel
-          label="Password"
-          id="password"
-          inputType="password"
-          placeholder="********"
-          name="userPassword"
-          value={inputs.userPassword}
-          onChange={handleChange}
-        />
-        <div className="flex items-center justify-between">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            disabled={btnDisable}
-            onClick={getLoginFormData}
-          >
-            Sign In
-          </button>
-          <button className="rounded-xl bg-gray-300 px-4 py-2">Reset </button>
-          <button
-            className="bg-red-700 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            onClick={props.signUpForm}
-          >
-            Signup
-          </button>
-        </div>
+        <form method="post" onSubmit={getLoginFormData}>
+          <InputWithLabel
+            label="Username"
+            id="username"
+            inputType="text"
+            placeholder="Username"
+            name="userName"
+            value={inputs.userName}
+            onChange={handleChange}
+          />
+          {error ? (
+            <p className=" text-red-800">
+              Please enter valid email and password
+            </p>
+          ) : (
+            ""
+          )}
+
+          <InputWithLabel
+            label="Password"
+            id="password"
+            inputType="password"
+            placeholder="********"
+            name="userPassword"
+            value={inputs.userPassword}
+            onChange={handleChange}
+          />
+          <div className="flex items-center justify-between">
+            <button
+              className={`text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+                !btnStatus
+                  ? "bg-blue-500 hover:bg-blue-700 cursor-pointer"
+                  : " bg-gray-500 cursor-not-allowed"
+              }`}
+              disabled={btnStatus}
+            >
+              Sign In
+            </button>
+            <button className="rounded-xl bg-gray-300 px-4 py-2">Reset </button>
+            <button
+              className="bg-red-700 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              onClick={props.signUpForm}
+            >
+              Signup
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
