f83e556c6d3782ba6bbb569c385c7fd955b0da4c
pankajwynk2020 <pankajkumar.tiwari@wynk.in>
2022-07-08 23:42:17 +0530

    remove conflicts

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import Products from "./components/product";
 import UsersList from "./pages/user/usersList";
 import SignupUser from "./components/UserSignupLogin/register";
 import ErrorBoundary from './components/errorBoundary';
-import ErrorBoundary from "./components/errorBoundary";
 const About = React.lazy(() => import('./pages/about'));
 const DynamicRouting = React.lazy(() => import('./components/dynamicRouting')); //lazy loading not default load page
 
